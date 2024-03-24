4d0089f15f6a8cd66e081bb89af2f00f8cbf9bf7
Aiman <65372518+Aiman7756@users.noreply.github.com>
2024-03-24 14:43:12 -0700

    some data entry

--- js/data.js
+++ js/data.js
@@ -1,4 +1,6 @@
 var data = {
+
+	
 	images: [
 		{
 			src: ["https://i.imgur.com/tByQnjx.png"],
@@ -28,9 +30,9 @@ var data = {
 			src: ["https://i.imgur.com/WrKKK3z.jpeg"],
 			thumbnail: "https://i.imgur.com/WrKKK3z.jpeg",
 			title: "",
-			artist: "",
-			artist_url: null,
-			art_url: null,
+			artist: "Nico",
+			artist_url: "https://twitter.com/Nico_squitter",
+			art_url: "https://twitter.com/Mina_776_/status/1726108770101833963/photo/1",
 			desc: "Birthday gift from @MisoSoup247",
 			date_str: "",
 			tags: [""],
@@ -40,9 +42,9 @@ var data = {
 			src: ["https://i.imgur.com/B1JNJxG.png"],
 			thumbnail: "https://i.imgur.com/B1JNJxG.png",
 			title: "",
-			artist: "",
-			artist_url: null,
-			art_url: null,
+			artist: "Shugo",
+			artist_url: "https://twitter.com/hiomaika",
+			art_url: "https://x.com/hiomaika/status/1708829194023813528?s=20",
 			desc: "Funded by the 3rd annual CYL Prediction Contest.",
 			date_str: "",
 			tags: [""],
@@ -53,8 +55,8 @@ var data = {
 			thumbnail: "https://i.imgur.com/jENQGj6.png",
 			title: "",
 			artist: "",
-			artist_url: null,
-			art_url: null,
+			artist_url: "https://twitter.com/BugLewdy",
+			art_url: "https://x.com/BugLewdy/status/1747415939992445194?s=20",
 			desc: "Birthday gift from @MrTalkingDinosaur",
 			date_str: "",
 			tags: [""],
@@ -66,6 +68,32 @@ var data = {
 			title: "",
 			artist: "",
 			artist_url: null,
+			art_url: null,
+			desc: "",
+			date_str: "",
+			tags: [""],
+			hidden: false
+		},
+
+		{
+			src: ["https://i.redd.it/6sgv0lp3hcpc1.jpeg"],
+			thumbnail: "https://i.redd.it/6sgv0lp3hcpc1.jpeg",
+			title: "",
+			artist: "",
+			artist_url: "https://twitter.com/moccha_kofii",
+			art_url: "https://x.com/moccha_kofii/status/1770137632112001409?s=20",
+			desc: "",
+			date_str: "",
+			tags: [""],
+			hidden: false
+		},
+
+		{
+			src: ["https://i.redd.it/5gsvhgm9gqoc1.png"],
+			thumbnail: "https://i.redd.it/5gsvhgm9gqoc1.png",
+			title: "",
+			artist: "DaphneComics",
+			artist_url: "https://twitter.com/DaphneComics",
 			art_url: null,
 			desc: "",
 			date_str: "",
