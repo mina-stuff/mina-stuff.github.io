9eb023e5dd6eecfd93a84fa8957e0e532b96e99e
Aiman <65372518+Aiman7756@users.noreply.github.com>
2024-04-10 22:38:24 -0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,105 +1,111 @@
 var data = {
-
-	
-	images: [
-		{
-			src: ["https://i.imgur.com/tByQnjx.png"],
-			thumbnail: "https://i.imgur.com/tByQnjx.png",
-			title: "",
-			artist: "",
-			artist_url: null,
-			art_url: null,
-			desc: "",
-			date_str: "",
-			tags: [""],
-			hidden: false
-		},
-				{
-			src: ["https://i.imgur.com/VKtQc1Z.jpeg"],
-			thumbnail: "https://i.imgur.com/VKtQc1Z.jpeg",
-			title: "",
-			artist: "",
-			artist_url: null,
-			art_url: null,
-			desc: "",
-			date_str: "",
-			tags: [""],
-			hidden: false
-		},
-				{
-			src: ["https://i.imgur.com/WrKKK3z.jpeg"],
-			thumbnail: "https://i.imgur.com/WrKKK3z.jpeg",
-			title: "",
-			artist: "Nico",
-			artist_url: "https://twitter.com/Nico_squitter",
-			art_url: "https://twitter.com/Mina_776_/status/1726108770101833963/photo/1",
-			desc: "Birthday gift from @MisoSoup247",
-			date_str: "",
-			tags: [""],
-			hidden: false
-		},
-				{
-			src: ["https://i.imgur.com/B1JNJxG.png"],
-			thumbnail: "https://i.imgur.com/B1JNJxG.png",
-			title: "",
-			artist: "Shugo",
-			artist_url: "https://twitter.com/hiomaika",
-			art_url: "https://x.com/hiomaika/status/1708829194023813528?s=20",
-			desc: "Funded by the 3rd annual CYL Prediction Contest.",
-			date_str: "",
-			tags: [""],
-			hidden: false
-		},
-				{
-			src: ["https://i.imgur.com/jENQGj6.png"],
-			thumbnail: "https://i.imgur.com/jENQGj6.png",
-			title: "",
-			artist: "",
-			artist_url: "https://twitter.com/BugLewdy",
-			art_url: "https://x.com/BugLewdy/status/1747415939992445194?s=20",
-			desc: "Birthday gift from @MrTalkingDinosaur",
-			date_str: "",
-			tags: [""],
-			hidden: false
-		},
-				{
-			src: ["https://i.imgur.com/TkdNzyE.jpeg"],
-			thumbnail: "https://i.imgur.com/TkdNzyE.jpeg",
-			title: "",
-			artist: "",
-			artist_url: null,
-			art_url: null,
-			desc: "",
-			date_str: "",
-			tags: [""],
-			hidden: false
-		},
-
-		{
-			src: ["https://i.redd.it/6sgv0lp3hcpc1.jpeg"],
-			thumbnail: "https://i.redd.it/6sgv0lp3hcpc1.jpeg",
-			title: "",
-			artist: "",
-			artist_url: "https://twitter.com/moccha_kofii",
-			art_url: "https://x.com/moccha_kofii/status/1770137632112001409?s=20",
-			desc: "",
-			date_str: "",
-			tags: [""],
-			hidden: false
-		},
-
-		{
-			src: ["https://i.redd.it/5gsvhgm9gqoc1.png"],
-			thumbnail: "https://i.redd.it/5gsvhgm9gqoc1.png",
-			title: "",
-			artist: "DaphneComics",
-			artist_url: "https://twitter.com/DaphneComics",
-			art_url: null,
-			desc: "",
-			date_str: "",
-			tags: [""],
-			hidden: false
-		}
-
-	]
+  images: [{
+      src: ["https://i.imgur.com/tByQnjx.png"],
+      thumbnail: "https://i.imgur.com/tByQnjx.png",
+      title: "Winter Mareeta",
+      artist: "",
+      artist_url: null,
+      art_url: null,
+      desc: "",
+      date_str: "",
+      tags: [],
+      hidden: false
+    },
+    {
+      src: ["https://i.imgur.com/VKtQc1Z.jpeg"],
+      thumbnail: "https://i.imgur.com/VKtQc1Z.jpeg",
+      title: "Sword Lilies",
+      artist: "",
+      artist_url: null,
+      art_url: null,
+      desc: "",
+      date_str: "",
+      tags: [],
+      hidden: false
+    },
+    {
+      src: ["https://i.imgur.com/WrKKK3z.jpeg"],
+      thumbnail: "https://i.imgur.com/WrKKK3z.jpeg",
+      title: "Garden Bench",
+      artist: "Nico",
+      artist_url: "https://twitter.com/Nico_squitter",
+      art_url: "https://twitter.com/Mina_776_/status/1726108770101833963/photo/1",
+      desc: "Birthday gift from @MisoSoup247",
+      date_str: "",
+      tags: [],
+      hidden: false
+    },
+    {
+      src: ["https://i.imgur.com/B1JNJxG.png"],
+      thumbnail: "https://i.imgur.com/B1JNJxG.png",
+      title: "Snow Feh",
+      artist: "Shugo",
+      artist_url: "https://twitter.com/hiomaika",
+      art_url: "https://x.com/hiomaika/status/1708829194023813528?s=20",
+      desc: "Funded by the 3rd annual CYL Prediction Contest.",
+      date_str: "",
+      tags: [],
+      hidden: false
+    },
+    {
+      src: ["https://i.imgur.com/jENQGj6.png"],
+      thumbnail: "https://i.imgur.com/jENQGj6.png",
+      title: "Eyvel & Mareeta",
+      artist: "BugLewdy",
+      artist_url: "https://twitter.com/BugLewdy",
+      art_url: "https://x.com/BugLewdy/status/1747415939992445194?s=20",
+      desc: "Birthday gift from @MrTalkingDinosaur",
+      date_str: "",
+      tags: [],
+      hidden: false
+    },
+    {
+      src: ["https://i.imgur.com/TkdNzyE.jpeg"],
+      thumbnail: "https://i.imgur.com/TkdNzyE.jpeg",
+      title: "Skeb",
+      artist: "",
+      artist_url: null,
+      art_url: null,
+      desc: "",
+      date_str: "",
+      tags: [],
+      hidden: false
+    },
+    {
+      src: ["https://i.redd.it/6sgv0lp3hcpc1.jpeg"],
+      thumbnail: "https://i.redd.it/6sgv0lp3hcpc1.jpeg",
+      title: "Mareeta & Ced",
+      artist: "Moccha Kofii",
+      artist_url: "https://twitter.com/moccha_kofii",
+      art_url: "https://x.com/moccha_kofii/status/1770137632112001409?s=20",
+      desc: "",
+      date_str: "March 19, 2024",
+      tags: [],
+      hidden: false
+    },
+    {
+      src: ["https://i.redd.it/5gsvhgm9gqoc1.png"],
+      thumbnail: "https://i.redd.it/5gsvhgm9gqoc1.png",
+      title: "Rally for Gaza 1",
+      artist: "DaphneComics",
+      artist_url: "https://twitter.com/DaphneComics",
+      art_url: null,
+      desc: "",
+      date_str: "March 15, 2024",
+      tags: [],
+      hidden: false
+    },
+    {
+      src: ["https://pbs.twimg.com/media/GKzqEtDboAAMfkV?format=jpg&name=large"],
+      thumbnail: "https://pbs.twimg.com/media/GKzqEtDboAAMfkV?format=jpg&name=large",
+      title: "Rally for Gaza 2",
+      artist: "zoedapoey",
+      artist_url: "https://twitter.com/zoedapoey",
+      art_url: "https://x.com/Mina_776_/status/1778053322311913568",
+      desc: "",
+      date_str: "April 10, 2024",
+      tags: [],
+      hidden: false
+    }
+  ]
 };
